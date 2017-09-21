8684e06ed7056171d86ccdb5943bdf28c518c589
StephanGoergen <stephan.goergen@gmail.com>
2017-09-21 17:18:54 -0400

    Add missing comma in classifiers.

--- setup.py
+++ setup.py
@@ -15,22 +15,22 @@ setup(
     include_package_data=False,
     keywords='board job search command line career developer engineer',
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Operating System :: OS Independent'
-        'Natural Language :: English',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
-        'Topic :: Office/Business',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+        'License :: OSI Approved :: MIT License'
+       ,'Development Status :: 3 - Alpha'
+       ,'Environment :: Console'
+       ,'Operating System :: OS Independent'
+       ,'Natural Language :: English'
+       ,'Intended Audience :: Developers'
+       ,'Intended Audience :: Information Technology'
+       ,'Intended Audience :: System Administrators'
+       ,'Intended Audience :: Science/Research'
+       ,'Topic :: Office/Business'
+       ,'Programming Language :: Python :: 2'
+       ,'Programming Language :: Python :: 2.7'
+       ,'Programming Language :: Python :: 3'
+       ,'Programming Language :: Python :: 3.3'
+       ,'Programming Language :: Python :: 3.4'
+       ,'Programming Language :: Python :: 3.5'
+    ]
 )
 
