8e48b0ba05835b1c487a54b0e118f52cf4216025
StephanGoergen <stephan.goergen@gmail.com>
2017-09-26 19:19:26 -0400

    Add script to automate deployment.

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/env/bash
+cd /Users/stephan/Projects/jobcli-app/
+
+SCRIPTVERSION=$(gsed -ne 's/.*version_option(\x27\([^ ]*\)\x27.*/\1/p' jobcli.py)
+SETUPVERSION=$(gsed -ne 's/.*version=\x27\([^ ]*\)\x27.*/\1/p' setup.py)
+
+# echo "script version: "$SCRIPTVERSION
+# echo "setup version: "$SETUPVERSION
+
+# Check that versions are consistent
+## in setup.py and jobcli.py
+if [ "$SCRIPTVERSION" != "$SETUPVERSION" ]
+then
+    echo 'Different version numbers in setup.py and jobcli.py'
+else
+    PIPVERSION=$(pip3 search jobcli | gsed -ne 's/^jobcli (\([^ ]*\)).*$/\1/p')
+    # echo "pip version: "$PIPVERSION
+
+    if [ "$SCRIPTVERSION" == "$PIPVERSION" ]
+    then
+        echo 'This version has already been deployed to PyPI'
+    else
+        echo 'Will deploy to pip' ; exit;
+        #1 Generate distribution and wheel
+        python3 setup.py sdist &&
+        python3 setup.py bdist_wheel --universal &&
+
+        #2 Upload to PyPI
+        twine upload dist/jobcli-0.1a2* &&
+
+        #3 pip update
+        pip3 install jobcli --upgrade
+    fi
+fi
+
+
+
+
